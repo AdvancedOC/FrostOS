df841dfc65e95b801d45728ff0586f36c83470f5
Blendi Goose <87442375+lieve-blendi@users.noreply.github.com>
2024-07-24 20:48:12 +0200

    yesing

--- os/kernel/utils.lua
+++ os/kernel/utils.lua
@@ -1,6 +1,7 @@
-function string.split(s, sep)
-
+function string.escape_pattern(text)
+    return text:gsub("([^%w])", "%%%1")
 end
+
 
 function string.contains(s, sub)
     return string.find(s, sub, nil, true) ~= nil
@@ -9,3 +10,5 @@ end
 function string.startswith(s,sub)
     return s:sub(1,#sub) == sub
 end
+
+function string.split(inputstr, sep) sep=string.escape_pattern(sep) local t={}  for field,s in string.gmatch(inputstr, "([^"..sep.."]*)("..sep.."?)") do table.insert(t,field)  if s=="" then return t end end end
