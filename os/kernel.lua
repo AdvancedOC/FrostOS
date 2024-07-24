45ba2190d1fbb2e30de9fbe0d7a9d8371c151add
Quantum Tomato <46377726+IonutParau@users.noreply.github.com>
2024-07-24 20:08:49 +0200

    Update kernel.lua

--- os/kernel.lua
+++ os/kernel.lua
@@ -1,2 +1,23 @@
 dofile("os/kernel/utils.lua")
 dofile("os/kernel/gio.lua")
+dofile("os/kernel/process.lua")
+dofile("os/kernel/pio.lua")
+
+AllDrivers = {}
+
+for _, driver in ipairs(gio.list("/os/drivers")) do
+  table.insert(AllDrivers, gio.dofile("/os/drivers/" .. driver))
+end
+
+-- For now, forcefully run bterm
+local bterm = Process.spawn("bterm", "/", nil, nil, nil, {}, 2)
+
+local success, err = bterm:exec("/os/bin/bterm.lua")
+
+if not success then
+  error(err)
+end
+
+while true do
+  coroutine.yield()
+end
