4fa2e7540963c90ca52f0149bc44feaf636f2f8a
Blendi Goose <87442375+lieve-blendi@users.noreply.github.com>
2024-07-28 21:08:44 +0200

    pastebin

---
+++ os/bin/pastebin.lua
@@ -0,0 +1,68 @@
+local args = ...
+local syscalls = require("syscalls")
+
+local pastebin = "https://pastebin.com/raw/"
+
+local function downloadFile(url)
+	local connection = syscalls.internet_request(url)
+	if not connection then
+		error("Unable to connect to " .. url)
+	end
+	connection.finishConnect()
+	local buf = ""
+	while true do
+		local data = connection.read(math.huge)
+		if not data then break end
+		buf = buf .. data
+	end
+	connection.close()
+	return buf
+end
+
+if args[1] == "run" then
+	local path = args[2]
+
+	print("Downloading file...")
+	local filedata,err = downloadFile(pastebin .. path)
+	if not filedata then error("Couldn't download file: " .. tostring(err)) end
+
+	local temppath = "/tmp/" .. path
+
+	print("Writing into " .. temppath .. "...")
+	local filehandle = io.open(temppath, "w")
+
+	if not filehandle then error("Can't open " .. temppath .. " for writing.") end
+
+	filehandle:write(filedata)
+	filehandle:close()
+
+	print("Spawning process...")
+	local process = require("process")
+	local child = process.spawn("pastebin run: " .. path)
+
+	print("Running file...")
+	local ok, procerr = process.exec(child, temppath)
+	if not ok then
+		error("Error: " .. procerr)
+	end
+	process.join(child)
+elseif args[1] == "get" then
+	local pastebinpath = args[2]
+	local filepath = args[3]
+
+	local filedata,err = downloadFile(pastebin .. pastebinpath)
+	if not filedata then error("Couldn't download file: " .. tostring(err)) end
+
+	if filepath then
+		local handle = io.open(filepath,"w")
+		if not handle then error("Could not open " .. filepath .. " for writing.") end
+
+		handle:write(filedata)
+		handle:close()
+
+		print("Successfully wrote the pastebin data to " .. filepath)
+	else
+		io.write(io.stdout, filedata)
+		io.flush()
+	end
+end
